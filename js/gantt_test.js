107ee8b9455ef664f9ee806acf6d3e77b065c8a6
jaeyo_on <jyo3on@gmail.com>
2018-05-29 13:12:31 +0900

    data

--- js/gantt_test.js
+++ js/gantt_test.js
@@ -4,15 +4,6 @@
 
 let colors = [ "FarRight", "MidRight", "Mid", "MidLeft", "FarLeft" ];
 
-let tasks = [
-    {
-        "startDate":new Date("Sun Dec 09 01:36:45 EST 2012"),
-        "endDate":new Date("Sun Dec 09 02:36:45 EST 2012"),
-        "color":"E Job",
-        "status":"RUNNING"
-    }
-];
-
 let taskStatus = {
     "FarRight" : "blue",
     "MidRight" : "blue-purple",
@@ -21,26 +12,30 @@ let taskStatus = {
     "FarLeft" : "red"
 };
 
-let taskSkull = {
-    "startDate" : makeYearMonth(0, 0), //d3.time.hour.offset(lastEndDate, Math.ceil(1 * Math.random())),
-    "endDate" : makeYearMonth(0, 0), // d3.time.hour.offset(lastEndDate, (Math.ceil(Math.random() * 3)) + 1),
-    "color" : colors[0], // 성향
-    "status" : taskStatus[0], // 성향별색상 데이터 분리 (중복데이터)
-    "location" : '',
-    "data" : {
-        "orgName": "color",
+let task_data = {
+        "orgName": "orgName",
         "orgEvent": [{
-            "name": "",
+            "name": "event1",
             "startDate": "",
             "endDate": ""
-        }, {
-            "name": "",
-            "startDate": "",
-            "endDate": ""
-        }],
+          }, {
+              "name": "",
+              "startDate": "",
+              "endDate": ""
+          }],
         "orgDescription": ""
+      };
+
+let tasks = [
+    {
+      "startDate" : makeYearMonth(0, 0), //d3.time.hour.offset(lastEndDate, Math.ceil(1 * Math.random())),
+      "endDate" : makeYearMonth(0, 0), // d3.time.hour.offset(lastEndDate, (Math.ceil(Math.random() * 3)) + 1),
+      "color" : colors[0], // 성향
+      "status" : taskStatus[0], // 성향별색상 데이터 분리 (중복데이터)
+      "location" : '',
+      "data" : task_data
     }
-};
+];
 
 let startYear = makeYearMonth(1910, 0);
 let endYear = makeYearMonth(1945, 0);
@@ -57,54 +52,8 @@ let format = "%H:%M";
 let timeDomainString = "1day";
 
 /*
- * Get Data
+ * Sort Data
  */
-
-d3.json("/js/data.json", function(error, data) {
-  if (error)
-    throw error;
-  console.log(data);
-});
-
-
-task_new = taskSkull
-task_new["startDate"] = makeYearMonth(1910, 0)
-task_new["endDate"] = makeYearMonth(1911, 2)
-task_new["status"] = colors[4]
-task_new["color"] = colors[4]
-task_new["data"] = {
-    "orgName": "테스트1",
-    "orgEvent": [{
-        "name": "사건1",
-        "startDate": makeYearMonth(1910, 1),
-        "endDate": makeYearMonth(1910, 11)
-    }, {
-        "name": "사건2",
-        "startDate": makeYearMonth(1911, 0),
-        "endDate": makeYearMonth(1911, 1)
-    }],
-    "orgDescription": "테스트 설명1"
-}
-
-task_new2 =  {
-    "startDate" : makeYearMonth(1910, 1), //d3.time.hour.offset(lastEndDate, Math.ceil(1 * Math.random())),
-    "endDate" : makeYearMonth(1911, 4), // d3.time.hour.offset(lastEndDate, (Math.ceil(Math.random() * 3)) + 1),
-    "color" : colors[2],
-    "status" : colors[2], // 데이터 분리 (중복데이터)
-    "data" : {
-        "orgName": "테스트2",
-        "orgEvent": [{
-            "name": "사건3",
-            "startDate": makeYearMonth(1910, 2),
-            "endDate": makeYearMonth(1910, 11)
-        }, {
-            "name": "사건4",
-            "startDate": makeYearMonth(1911, 0),
-            "endDate": makeYearMonth(1911, 2)
-        }],
-        "orgDescription": "테스트 설명1"
-    }
-}
 
 // tasks.sort(function(a, b) {
 //     return a.endDate - b.endDate;
@@ -117,23 +66,50 @@ task_new2 =  {
 // let minDate = tasks[0].startDate;
 
 
-
 /*
  * Draw Chart
  */
 
 let gantt = d3.gantt().taskTypes(colors).taskStatus(taskStatus).tickFormat(format).height(document.querySelector("#chart").offsetHeight - 80).width(5000);
 
-
 gantt.timeDomainMode("fixed");
 gantt.timeDomain([ d3.time.month.offset(Date.UTC(year=1910,0,0,0,0), 0), Date.UTC(year=1945,0,0,0,0) ]);
 gantt.tickFormat("%m")
 console.log([ d3.time.month.offset(Date.UTC(year=1910,0,0,0,0), 0), Date.UTC(year=1945,0,0,0,0) ])
 
-// changeTimeDomain(timeDomainString);
-tasks = [task_new, task_new2]
+//changeTimeDomain(timeDomainString);
+//gantt(tasks);
 gantt(tasks);
-// gantt()
+
+d3.json("https://raw.githubusercontent.com/jyoonsong/hci-project/master/js/data.json", function(error, data) {
+  if (error)
+    throw error;
+  for (let i = 0; i < data.length; i++) {
+    let start = data[i].startDate.split("/");
+    let end = data[i].endDate.split("/");
+    
+    
+    data[i].startDate = makeYearMonth(start[0], start[1]);
+    data[i].endDate = makeYearMonth(end[0], end[1]);
+    
+    data[i].data.orgEvent.forEach( function(e) { 
+      console.log("hihi");console.log(e);
+      let eventStart = e.startDate.split("/");
+      let eventEnd = e.endDate.split("/");
+      e.startDate = makeYearMonth(eventStart[0], eventStart[1]);
+      e.endDate = makeYearMonth(eventEnd[0], eventEnd[1]);
+      console.log("hihi");console.log(e);
+    });
+    
+    addTask(data[i]);
+  }
+});
+//
+//addTask(makeYearMonth(1910, 3), makeYearMonth(1915, 5), colors[0], task_data);
+//addTask(makeYearMonth(1910, 4), makeYearMonth(1913, 7), colors[1], task_data);
+//addTask(makeYearMonth(1910, 6), makeYearMonth(1914, 9), colors[2], task_data);
+
+console.log(tasks);
 
 
 /*
@@ -141,9 +117,7 @@ gantt(tasks);
 */
 
 function changeTime(idx) {
-    console.log( d3.time.month.offset(PossibleTimeDomain[idx]['startDate'], 0),
-        PossibleTimeDomain[idx]['endDate']);
-
+//    console.log( d3.time.month.offset(PossibleTimeDomain[idx]['startDate'], 0), PossibleTimeDomain[idx]['endDate']);
     document.querySelectorAll('[class="scrollable"]')[0].scrollLeft = 0
 
     gantt.timeDomain([ d3.time.month.offset(PossibleTimeDomain[idx]['startDate'], 0),
@@ -195,18 +169,8 @@ function getEndDate() {
 }
 
 let i = 0
-function addTask(startMonth, endMonth, color = colors[0], status = "MidRight") {
-    tasks.push({
-        "startDate" : startMonth, //d3.time.hour.offset(lastEndDate, Math.ceil(1 * Math.random())),
-        "endDate" : endMonth, // d3.time.hour.offset(lastEndDate, (Math.ceil(Math.random() * 3)) + 1),
-        "color" : color,
-        "status" : status,
-        "data" : {
-            "orgName": "test1",
-            "orgEvent": [],
-            "orgDescription": ""
-        }
-    });
+function addTask(task) {
+    tasks.push(task);
 
     // changeTimeDomain(timeDomainString);
     gantt.redraw(tasks);
@@ -231,9 +195,3 @@ function addRandomHistory() {
 function makeYearMonth(y, m) {
     return Date.UTC(y, m, 0, 0, 0)
 }
-
-addTask(makeYearMonth(1910, 3), makeYearMonth(1910, 5), colors[0], colors[0])
-addTask(makeYearMonth(1910, 4), makeYearMonth(1910, 7), colors[1], colors[1])
-addTask(makeYearMonth(1910, 6), makeYearMonth(1910, 9), colors[0], colors[0])
-
-console.log(tasks)
